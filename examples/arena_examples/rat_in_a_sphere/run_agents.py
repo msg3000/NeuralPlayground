3ff9bef0766bfadc57bd35907841343fc2cf46a2
MSG <muhammadsahalg@gmail.com>
2024-09-12 15:28:02 +0200

    changes

--- examples/arena_examples/rat_in_a_sphere/run_agents.py
+++ examples/arena_examples/rat_in_a_sphere/run_agents.py
@@ -100,9 +100,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     gravity_mag = args.gravity_mag
 
-    print("========= Beginning spherical agent =========== ")
-    run_spherical_agent(gravity_mag=gravity_mag)
-    print("========= Spherical agent complete =========== ")
+    # print("========= Beginning spherical agent =========== ")
+    # run_spherical_agent(gravity_mag=gravity_mag)
+    # print("========= Spherical agent complete =========== ")
     print("========= Beginning orthogonal projection agent =========== ")
     run_ortho_proj_agent(gravity_mag=gravity_mag)
     print("========= Orthogonal projection agent complete =========== ")
